d85c8b2c87ac2e567ce6efae284fb58af6af6f8c
nickvflorez <nickvflorez@Florez-ASU.3520.dhcp.asu.edu>
2019-02-25 12:23:02 -0700

    third commit

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,37 +1,58 @@
 package cse360assign2;
+//Nickolas Florez
+//CSE360 Assignment 2
+//Lecture: Mondays 3:05-4:20
+//PIN: 375
 
 public class Calculator {
-	private int total;
-	
-	public Calculator () {
+	private int total; //total of method calculations
+	private String history;
+
+	public Calculator() {
 		total = 0;  // not needed - included for clarity
+		history = "0 ";
 	}
 	
-	public int getTotal () {
+	public int getTotal() { //should grab the total after methods used so far
 		return total;
+
 	}
 	
-	public void add (int value) {
-		total = total + value;
+	public void add(int value) {
+		total = total + value; //add value to the total
+		String val = Integer.toString(value); //cast int to String
+		history += "+ " + val + " "; 
 	}
 	
-	public void subtract (int value) {
-		total = total - value;
+	public void subtract(int value) {
+		total = total - value; //subtract the value from the total
+		String val = Integer.toString(value);
+		history += "- " + val + " ";
+
 	}
 	
-	public void multiply (int value) {
-		total = total * value;
+	public void multiply(int value) {
+		total = total * value; //multiply the value by the total
+		String val = Integer.toString(value);
+		history += "* " + val + " ";
+
 	}
-	
-	public void divide (int value) {
-		total = total / value;
+	//if value is equal to 0, then total = 0
+	//otherwise, divide value by the total
+	public void divide(int value) {
 		if(value == 0) {
 			total = 0;
+		} 
+		else {
+			total = total / value;
 		}
+		String val = Integer.toString(value); 
+		history += "/ " + val + " ";	
+
 	}
-	
-	public String getHistory () {
-		return "";
+	//getHistory() will get the history values used in each 
+	//successive method used. 
+	public String getHistory() {
+		return history;	
 	}
-	
 }
